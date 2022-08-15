a00075c367cfa7e8451be8c29fe444c6f2bdbc82
javidVeg <javidx24@icloud.com>
2022-08-15 07:40:36 -0400

    added fixed navbar

--- Client/src/App.js
+++ Client/src/App.js
@@ -12,6 +12,7 @@ import { useInView } from "react-intersection-observer";
 import { Reviews } from "./components/Reviews";
 import { Photos } from "./components/Photos";
 import Sand from "../src/assets/sand.jpg";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   const { ref, inView } = useInView();
@@ -39,48 +40,53 @@ function App() {
 
   //! @ -------------------------------------------------------------|
   return (
-    <div className="flex flex-col max-w-full overflow-hidden">
-      <section className="h-screen box-border ">
-        <video src={videoBG} autoPlay loop muted />
-        <div className="absolute w-full h-screen top-0 flex flex-col items-center ">
-          <div className="absolute top-0 left-0 w-screen h-screen bg-black bg-opacity-40 "></div>
-          <div className="absolute grid scale-110 bottom-10 h-screen place-items-center md:h-screen md:scale-100 md:bottom-0">
-            <img src={AffinitiLogo} alt="logo" width="80%" />
-          </div>
-        </div>
+    <div>
+      <section className="">
+        <Navbar/>
       </section>
-      <section>
-        <div className="flex flex-col justify-center items-center">
-          <div className="grid place-content-center mb-5 mt-20">
-            <About />
+      <div className="flex flex-col max-w-full overflow-hidden">
+        <section className="h-screen box-border ">
+          <video src={videoBG} autoPlay loop muted />
+          <div className="absolute w-full h-screen top-0 flex flex-col items-center ">
+            <div className="absolute top-6 left-0 w-screen h-screen bg-black bg-opacity-40 "></div>
+            <div className="absolute grid scale-110 bottom-10 h-screen place-items-center md:h-screen md:scale-100 md:bottom-0">
+              <img src={AffinitiLogo} alt="logo" width="80%" />
+            </div>
           </div>
-          <div className="mt-10 grid place-content-center ">
-            <Photos />
+        </section>
+        <section>
+          <div className="flex flex-col justify-center items-center">
+            <div className="grid place-content-center mb-5 mt-20">
+              <About />
+            </div>
+            <div className="mt-10 grid place-content-center ">
+              <Photos />
+            </div>
+            <div className="mt-10 grid place-content-center ">
+              <Reviews />
+            </div>
           </div>
-          <div className="mt-10 grid place-content-center ">
-            <Reviews />
+          <div className="mt-20 md:mt-40">
+            <StylistCard />
           </div>
+        </section>
+        <div className=" mt-20 static grid grid-cols-1 place-items-center -mb-11">
+          <img src={Sand} className="relative h-screen w-screen " />
+          <motion.div
+            ref={ref}
+            animate={animation}
+            className="absolute bg-white rounded overflow-hidden shadow-lg justify-center "
+          >
+            <div className="m-5 md:m-10 gap-5 flex flex-col md:flex-row items-center">
+              <LocationCard />
+              <BookNowButton />
+            </div>
+          </motion.div>
         </div>
-        <div className="mt-20 md:mt-40">
-          <StylistCard />
-        </div>
-      </section>
-      <div className=" mt-20 static grid grid-cols-1 place-items-center -mb-11">
-        <img src={Sand} className="relative h-screen w-screen " />
-        <motion.div
-          ref={ref}
-          animate={animation}
-          className="absolute bg-white rounded overflow-hidden shadow-lg justify-center "
-        >
-          <div className="m-5 md:m-10 gap-5 flex flex-col md:flex-row items-center">
-            <LocationCard />
-            <BookNowButton />
-          </div>
-        </motion.div>
+        <footer className="mt-10">
+          <Footer />
+        </footer>
       </div>
-      <footer className="mt-10">
-        <Footer />
-      </footer>
     </div>
   );
 }
---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+  
+    return (
+    <div>
+        <div className="w-full h-20 fixed z-10 bg-gray-700 bg-opacity-80 shadow-md bg-clip-padding navbar-blur">
+            <nav className="relative flex items-center justify-between w-full max-w-screen-xl text-white">
+NavBar
+            </nav>
+        </div>
+    </div>
+  )
+}
