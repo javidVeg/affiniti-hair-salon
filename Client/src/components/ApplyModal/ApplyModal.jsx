7a859c4b8159b5b689c28e684d607808ad149815
David_Martinez <javidx24@icloud.com>
2023-01-02 19:04:27 -0500

    edits to modal form_v2

--- Client/src/components/ApplyModal/ApplyModal.jsx
+++ Client/src/components/ApplyModal/ApplyModal.jsx
@@ -32,29 +32,48 @@ const ApplyModal = ({ closeModal }) => {
         <div className="modal-title">Join the team!</div>
         <div className="modal-form">
           <form>
-            <div className="name">
+            <div className="inputs">
               <div>
                 <label htmlFor="first-name">First Name</label>
                 <input type="text" id="first-name" name="firstname" />
               </div>
               <div>
                 <label htmlFor="last-name">Last Name</label>
-                <input type="text" id="last-name" name="lastname"  />
+                <input type="text" id="last-name" name="lastname" />
               </div>
             </div>
-            <label htmlFor="email">Email Address</label>
-            <input type="text" id="email" name="email" placeholder='Email address..' />
-            <label htmlFor="phone">Phone Number</label>
-            <input type="text" id="phone" name="phone" placeholder='Phone number..' />
-            <label htmlFor="role">Role</label>
-            <select id="role" name="roles">
+            <div className="inputs">
+              <div>
+                <label htmlFor="email">Email Address</label>
+                <input type="text" id="email" name="email" />
+              </div>
+            </div>
+            <div className="inputs">
+              <div>
+                <label htmlFor="phone">Phone Number</label>
+                <input type="text" id="phone" name="phone" />
+              </div>
+            </div>
+
+            <label htmlFor="role" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">What Role are you interested in?</label>
+            <select id="role" name="roles" className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+              <option selected value="Role of Interest">Choose a role</option>
               <option value="Hair Stylist">Hair Stylist</option>
               <option value="Hair Colorist">Hair Colorist</option>
               <option value="Lash Artist">Lash Artist</option>
             </select>
-            <label htmlFor="ig">Instagram</label>
-            <input type="text" id="ig" name="ig" placeholder='Instagram' />
-            <textarea id="more-info" name="moreinfo" maxLength={100} placeholder="Who are some of your favorite stylist and why?"></textarea>
+
+            <div className="inputs">
+              <div>
+                <label htmlFor="ig">Instagram</label>
+                <input type="text" id="ig" name="ig" />
+              </div>
+            </div>
+            <textarea id="more-info" name="moreinfo" maxLength={100}
+              placeholder="Who are some of your favorite stylist and why?"
+              rows="3"
+              class="block p-2.5 w-full text-sm">
+            </textarea>
             <input onClick={handleClick} type="submit" value="Submit" />
           </form>
         </div>
